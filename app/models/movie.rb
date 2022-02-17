ce22d940a0f6766cfe678c7f9e7793c67a656c2c
afonso miguel <afonso.miguel1995@gmail.com>
2022-02-17 12:40:04 +0000

    added validations and associations to the models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+  has_many :lists, through: :bookmarks
+  validates :title, uniqueness: true
+  validates :overview, :title, presence: true
 end
